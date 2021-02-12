1095adf87e147389ba7cd9b4490df8215469eaa5
Mike Lay <mike@mkly.io>
2021-02-11 16:34:41 -0800

    Add header

---
+++ docs/header-footer.js
@@ -0,0 +1,53 @@
+const SAHeader = `<main><section class="hero-unit container" data-hero-unit="">
+  <div class="container-inner hero-unit__content" data-search-box="">
+    <div class="breadcrumbs-wrapper">
+      <ol class="breadcrumbs">
+  
+    <li title="CircleCI Support Center">
+      
+        <a href="/hc/en-us">CircleCI Support Center</a>
+      
+    </li>
+  
+    <li title="Mobile">
+      
+        <a href="/hc/en-us/categories/115001914008-Mobile">Mobile</a>
+      
+    </li>
+  
+    <li title="macOS iOS Xcode">
+      
+        <a href="/hc/en-us/sections/115003327088-macOS-iOS-Xcode">macOS iOS Xcode</a>
+      
+    </li>
+  
+</ol>
+
+    </div>
+    <h1 class="hero-unit__title">
+     Conditionally utilize different Xcode provisioning profiles
+    </h1>
+    <div class="article-meta">
+      <div class="article-meta__col article-meta__col--main">
+        <div class="entry-info ">
+          <div class="entry-info__avatar">
+            <div class="avatar">
+              <img class="user-avatar user-avatar--default" src="https://support.circleci.com/system/photos/361372847093/download.jpeg" alt="Avatar">
+            </div>
+          </div>
+          <div class="entry-info__content">
+            <a target="_zendesk_lotus" class="author author--hero-unit" href="/access/return_to?return_to=https://circleci.zendesk.com/agent/users/405652843914/tickets">
+              Nick
+            </a>
+            <div class="meta meta--hero-unit"><time datetime="2021-02-11T16:28:27Z" title="2021-02-11 08:28" data-datetime="calendar">Today at 08:28</time></div>
+          </div>
+        </div>
+      </div>
+      <div class="article-meta__col article-meta__col--button">
+        <div><button data-selected="false" aria-expanded="false" aria-haspopup="menu" type="button" id="187d2407-408f-4608-9da7-ba853c6c72ab">Follow</button></div>
+      </div>
+    </div>
+  </div>
+</section><div class="article-page__container"><article class="article clearfix"><div class="article__body markdown">`;
+
+SAFooter = `</div></article></div></main>`;
